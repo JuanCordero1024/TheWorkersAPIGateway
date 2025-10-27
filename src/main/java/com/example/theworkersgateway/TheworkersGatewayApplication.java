13302b0f3309de370f0ec54e827cbee3850c9ad3
juanCordero99 <juan.pablo.cor.sot@gmail.com>
2025-10-26 23:36:07 -0600

    feat: First commit and first implementation of one external API

---
+++ src/main/java/com/example/theworkersgateway/TheworkersGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.theworkersgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheworkersGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheworkersGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/theworkersgateway/TheworkersGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.theworkersgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheworkersGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
